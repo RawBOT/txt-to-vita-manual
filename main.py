ba0185cd48f0e1b074f792ef878f0b017e9bfe5b
RawBOT <rodwilhelmy@gmail.com>
2021-09-29 23:52:23 +0200

    Added multiple output image configurations (size, rotated, etc.)

--- main.py
+++ main.py
@@ -3,30 +3,54 @@
 import math, os, sys
 from PIL import Image, ImageDraw, ImageFont
 
-# Image Size = 960x544
-img_size = (960, 544)
-max_imgs = 999
+class image_config:
+    def __init__(self, image_size, font_size, max_lines_per_image, start_coord, rotate) -> None:
+        self.image_size = image_size
+        self.font_size = font_size
+        self.max_lines_per_image = max_lines_per_image
+        self.start_coord = start_coord
+        self.rotate = rotate
 
-# Iosevka Fixed 22pt (80x22)
+max_imgs = 999
 font_path = "fonts/iosevka-fixed-regular.ttf"
-font_size = 22
 fgcolor = "white"
 bgcolor = "black"
-start_coord = (35,30)
-max_lines_per_image = 22
-
-# Other params
 max_chars_per_line = 80
 output_dir = "Manual/"
+line_tracker_font_size = 11
 
+def fullscreen_image_config():
+    return image_config((960, 544), 21, 23, (40,28), False)
+
+def native_width_scrollable_image_config():
+    return image_config((960, 750), 21, 33, (40,26), False)  # native width
+
+def max_height_scrollable_image_config():
+    return image_config((480, 1500), 9, 161, (40,26), False)  # max height
+
+def min_width_scrollable_image_config():
+    return image_config((544, 1420), 11, 124, (33,26), False)  # "min" width / max height
+
+def best_width_scrollable_image_config():
+    return image_config((720, 1072), 15, 68, (40,26), False)  # 720 for less blurriness
+
+def vertical_image_config():
+    return image_config((544, 960), 11, 82, (33,30), True)
 
 if __name__ == "__main__":
+    config = fullscreen_image_config()
+    # config = native_width_scrollable_image_config()
+    # config = max_height_scrollable_image_config()
+    # config = min_width_scrollable_image_config()
+    # config = best_width_scrollable_image_config()
+    # config = vertical_image_config()
+
     os.makedirs(output_dir, exist_ok=True)
     txt_file_path = sys.argv[1]
 
     # Load fonts
-    normal_font = ImageFont.truetype(font_path, size=font_size)  # font used for guide text
-    half_font = ImageFont.truetype(font_path, size=11)  # font used for top-right line idx
+    normal_font = ImageFont.truetype(font_path, size=config.font_size)  # font used for guide text
+    half_font = ImageFont.truetype(font_path, size=line_tracker_font_size)  # font used for top-right line idx
 
     # Open the file with UTF-8 encoding; if it fails, then with default
     try:
@@ -41,11 +65,11 @@ if __name__ == "__main__":
 
     try:
         num_lines = len(txt_content)  # num. lines in the text guide
-        num_pages = math.ceil(num_lines/max_lines_per_image)  # num. of pages/images (rounded up) in the vita guide
+        num_pages = math.ceil(num_lines/config.max_lines_per_image)  # num. of pages/images (rounded up) in the vita guide
 
         # Check if predicted number of pages exceed Vita's max of {max_imgs}
         if num_pages > max_imgs:
-            max_lines = max_imgs*max_lines_per_image
+            max_lines = max_imgs*config.max_lines_per_image
             print("ERROR:The text file exceeds the maximum length allowed!")
             print("      Current: {0}, Max: {1} lines".format(num_lines, max_lines))
             quit()
@@ -55,28 +79,31 @@ if __name__ == "__main__":
             print("Progress: {0} / {1}".format(current_page+1, num_pages))
 
             # Initialize image
-            image = Image.new("RGB", img_size, bgcolor)
+            image = Image.new("RGB", config.image_size, bgcolor)
             draw = ImageDraw.Draw(image)
 
             # Draw border
-            border_coord = [(int(start_coord[0] / 2), int(start_coord[1] / 2)), \
-                            (int(img_size[0] - (start_coord[0] / 2)), int(img_size[1] - (start_coord[1] / 2)))]
+            border_coord = [(int(config.start_coord[0] / 2), int(config.start_coord[1] / 2)), \
+                            (int(config.image_size[0] - (config.start_coord[0] / 2)),
+                             int(config.image_size[1] - (config.start_coord[1] / 2)))]
             draw.rectangle(border_coord, outline=fgcolor, width=2)
 
             # line ranges for image
-            current_line = current_page * max_lines_per_image
-            line_idx = slice(current_line, min(current_line + max_lines_per_image, num_lines))
+            current_line = current_page * config.max_lines_per_image
+            line_idx = slice(current_line, min(current_line + config.max_lines_per_image, num_lines))
 
             # Draw top-right lines idx text: "{current_line} / {num_lines}"
-            text_coord = (int(img_size[0] - (start_coord[0] / 2)), 2)
+            text_coord = (int(config.image_size[0] - (config.start_coord[0] / 2)), 2)
             draw.text(text_coord, "{0} / {1}".format(line_idx.start, num_lines), \
                       font=half_font, fill=fgcolor, anchor="rt")  # anchored to right-top
 
             # Draw lines contained in image from: current_line -> {current_line} + {max_lines_per_image}
             drawn_text = ''.join(txt_content[line_idx]).expandtabs()  # Expand tabs to match original look
-            draw.multiline_text(start_coord, drawn_text, font=normal_font, fill=fgcolor, spacing=0)
+            draw.multiline_text(config.start_coord, drawn_text, font=normal_font, fill=fgcolor, spacing=0)
 
             # Save image with a [000].png format
+            if config.rotate:
+                image = image.rotate(90, expand=True)
             image.save(output_dir + "{0:03d}".format(current_page+1) + ".png")
     finally:
         input_file.close()
