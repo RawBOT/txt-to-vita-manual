145b45254919d427829e1f78b496cc46ec612e4a
RawBOT <rodwilhelmy@gmail.com>
2021-10-04 21:38:40 +0200

    Moved `finally` block to try-catch for file open

--- main.py
+++ main.py
@@ -106,50 +106,49 @@ if __name__ == "__main__":
     except:
         print("ERROR: Unknown file encoding!")
         quit()
-
-    try:
-        num_lines = len(txt_content)  # num. lines in the text guide
-        num_pages = math.ceil(num_lines/config.max_lines_per_image)  # num. of pages/images (rounded up) in the vita guide
-
-        # Check if predicted number of pages exceed Vita's max of {max_imgs}
-        if num_pages > max_imgs:
-            max_lines = max_imgs*config.max_lines_per_image
-            print("ERROR:The text file exceeds the maximum length allowed!")
-            print("      Current: {0}, Max: {1} lines".format(num_lines, max_lines))
-            quit()
-
-        # main loop
-        for current_page in range(num_pages):
-            # Initialize image
-            image = Image.new("RGB", config.image_size, bgcolor)
-            draw = ImageDraw.Draw(image)
-
-            # Draw border
-            border_coord = [(int(config.start_coord[0] / 2), int(config.start_coord[1] / 2)), \
-                            (int(config.image_size[0] - (config.start_coord[0] / 2)),
-                             int(config.image_size[1] - (config.start_coord[1] / 2)))]
-            draw.rectangle(border_coord, outline=fgcolor, width=2)
-
-            # line ranges for image
-            current_line = current_page * config.max_lines_per_image
-            line_idx = slice(current_line, min(current_line + config.max_lines_per_image, num_lines))
-
-            # Draw top-right lines idx text: "{current_line} / {num_lines}"
-            text_coord = (int(config.image_size[0] - (config.start_coord[0] / 2)), 2)
-            draw.text(text_coord, "{0} / {1}".format(line_idx.start, num_lines), \
-                      font=half_font, fill=fgcolor, anchor="rt")  # anchored to right-top
-
-            # Draw lines contained in image from: current_line -> {current_line} + {max_lines_per_image}
-            drawn_text = ''.join(txt_content[line_idx]).expandtabs()  # Expand tabs to match original look
-            draw.multiline_text(config.start_coord, drawn_text, font=normal_font, fill=fgcolor, spacing=0)
-
-            # Save image with a [000].png format
-            if config.rotate:
-                image = image.rotate(90, expand=True)
-            output_path = options.output_dir + "{0:03d}".format(current_page+1) + ".png"
-            image.save(output_path)
-
-            if options.verbose == True: print("Writing: {0}".format(output_path))
-            print("Progress: {0} / {1}".format(current_page+1, num_pages))
     finally:
         input_file.close()
+
+    num_lines = len(txt_content)  # num. lines in the text guide
+    num_pages = math.ceil(num_lines/config.max_lines_per_image)  # num. of pages/images (rounded up) in the vita guide
+
+    # Check if predicted number of pages exceed Vita's max of {max_imgs}
+    if num_pages > max_imgs:
+        max_lines = max_imgs*config.max_lines_per_image
+        print("ERROR:The text file exceeds the maximum length allowed!")
+        print("      Current: {0}, Max: {1} lines".format(num_lines, max_lines))
+        quit()
+
+    # main loop
+    for current_page in range(num_pages):
+        # Initialize image
+        image = Image.new("RGB", config.image_size, bgcolor)
+        draw = ImageDraw.Draw(image)
+
+        # Draw border
+        border_coord = [(int(config.start_coord[0] / 2), int(config.start_coord[1] / 2)), \
+                        (int(config.image_size[0] - (config.start_coord[0] / 2)),
+                            int(config.image_size[1] - (config.start_coord[1] / 2)))]
+        draw.rectangle(border_coord, outline=fgcolor, width=2)
+
+        # line ranges for image
+        current_line = current_page * config.max_lines_per_image
+        line_idx = slice(current_line, min(current_line + config.max_lines_per_image, num_lines))
+
+        # Draw top-right lines idx text: "{current_line} / {num_lines}"
+        text_coord = (int(config.image_size[0] - (config.start_coord[0] / 2)), 2)
+        draw.text(text_coord, "{0} / {1}".format(line_idx.start, num_lines), \
+                    font=half_font, fill=fgcolor, anchor="rt")  # anchored to right-top
+
+        # Draw lines contained in image from: current_line -> {current_line} + {max_lines_per_image}
+        drawn_text = ''.join(txt_content[line_idx]).expandtabs()  # Expand tabs to match original look
+        draw.multiline_text(config.start_coord, drawn_text, font=normal_font, fill=fgcolor, spacing=0)
+
+        # Save image with a [000].png format
+        if config.rotate:
+            image = image.rotate(90, expand=True)
+        output_path = options.output_dir + "{0:03d}".format(current_page+1) + ".png"
+        image.save(output_path)
+
+        if options.verbose == True: print("Writing: {0}".format(output_path))
+        print("Progress: {0} / {1}".format(current_page+1, num_pages))
