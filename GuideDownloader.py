0d037612f6cce6cddad9556176e00264ee9f235e
RawBOT <rodwilhelmy@gmail.com>
2021-10-04 21:39:56 +0200

    Add new module for downloading guides from the internet

---
+++ GuideDownloader.py
@@ -0,0 +1,28 @@
+from urllib import parse
+import requests
+from bs4 import BeautifulSoup
+
+request_headers = {"user-agent": "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"}
+
+class GuideContent:
+    def __init__(self, content, is_textguide):
+        self.content = content
+        self.is_textguide = is_textguide
+
+def is_remote_url(url):
+    decomposed_url = parse.urlparse(url)
+    return decomposed_url.scheme in ["http" ,"https"]
+
+def download_guide(url):
+    response = requests.Session().get(url, headers=request_headers)
+    soup = BeautifulSoup(response.text, features="html.parser")
+
+    # Parsing guide content
+    content = soup.select_one(".faqtext")
+    is_textguide = content != None
+    if(is_textguide):
+        lines = content.text.splitlines(keepends=True)
+        return GuideContent(lines, True)
+    else:
+        # Implement HTML guide parsing
+        return GuideContent([], False)
